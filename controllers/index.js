56e66e6b61637a12f307f20b8a5362324d9a7c16
Jbradu2 <Joshua.ry.bradshaw@gmail.com>
2023-12-29 15:10:22 -0600

    added basic indexing code

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const userController = require('./user-controller');
+const thoughtController = require('./thought-controller');
+const reactionController = require('./reaction-controller');
+
+module.exports = {
+  userController,
+  thoughtController,
+  reactionController,
+};
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+const reactionRoutes = require('./reaction-routes');
+
+module.exports = {
+  userRoutes,
+  thoughtRoutes,
+  reactionRoutes,
+};
